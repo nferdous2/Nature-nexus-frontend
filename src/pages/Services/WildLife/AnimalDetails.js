ce79ca4b4d7b5a37a1defe1fc158cd097743dd12
jannat456nur <jannatjenny22@gmail.com>
2023-11-17 11:29:19 +0600

    add the adopt form and active share button

--- src/pages/Services/WildLife/AnimalDetails.js
+++ src/pages/Services/WildLife/AnimalDetails.js
@@ -6,17 +6,21 @@ import CardMedia from '@mui/material/CardMedia'
 import CardContent from '@mui/material/CardContent'
 import CardActions from '@mui/material/CardActions'
 import Collapse from '@mui/material/Collapse'
-import Avatar from '@mui/material/Avatar'
 import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
-import { red } from '@mui/material/colors'
-import FavoriteIcon from '@mui/icons-material/Favorite'
-import ShareIcon from '@mui/icons-material/Share'
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  Snackbar,
+} from '@mui/material'
+
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import MoreVertIcon from '@mui/icons-material/MoreVert'
-import { useLocation, useNavigate, useParams } from 'react-router'
-import { Button } from '@mui/material'
-import { useHistory } from 'react-router-dom'
+import ShareIcon from '@mui/icons-material/Share'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle'
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props
@@ -32,69 +36,83 @@ const ExpandMore = styled((props) => {
 export default function AnimalDetails() {
   const [expanded, setExpanded] = React.useState(false)
   const [animal, setAnimal] = React.useState({})
-  // const { id } = useParams()
+  const [openModal, setOpenModal] = React.useState(false)
+  const [input1, setInput1] = React.useState('')
+  const [input2, setInput2] = React.useState('')
+  const [input3, setInput3] = React.useState('')
+  const [input4, setInput4] = React.useState('')
+  const [snackbarOpen, setSnackbarOpen] = React.useState(false)
+  const [saveSuccessSnackbarOpen, setSaveSuccessSnackbarOpen] = React.useState(
+    false,
+  )
 
-  // const { myState } = location.state
-  // console.log(props.location.state)
-  // console.log(myState)
   React.useEffect(() => {
-    //setitem use for set
-    //getitem is used for get the animals
     const animalData = localStorage.getItem('animal')
     setAnimal(JSON.parse(animalData))
   }, [])
 
-  console.log(animal.name)
-
   const handleExpandClick = () => {
     setExpanded(!expanded)
   }
-  const navigate = useNavigate()
 
-  // Function to navigate to the adoptform page
-  const navigateToAdoptForm = () => {
-    // Replace '/adoptform' with the actual path of your adoptform page
-    navigate('/adoptform')
+  const handleOpenModal = () => {
+    setOpenModal(true)
   }
-  // React.useEffect(() => {
-  //   // Fetch the animal data based on the ID from your JSON file or API
-  //   fetch(`./wildlifeAnimalData.JSON=${id}`)
-  //     .then((res) => res.json())
-  //     .then((data) => setAnimal(data))
-  // }, [id])
-  // const [userData, setUserData] = React.useState(null);
 
-  // React.useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch('./wildlifeAnimalData.JSON') // Adjust the path accordingly
-  //       const data = await response.text()
+  const handleCloseModal = () => {
+    setOpenModal(false)
+  }
 
-  //       const user = data.find((user) => user.id == Number(id))
+  const handleSaveModal = () => {
+    // Add logic to save input values or perform any other actions
+    console.log('Input 1:', input1)
+    console.log('Input 2:', input2)
+    console.log('Input 3:', input3)
+    console.log('Input 4:', input4)
 
-  //       if (user) {
-  //         setAnimal(user)
-  //       } else {
-  //         console.log('User not found')
-  //       }
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error)
-  //     }
-  //   }
+    // Show Snackbar for save success
+    setSaveSuccessSnackbarOpen(true)
 
-  //   fetchData()
-  // }, [id])
+    // Close the modal
+    handleCloseModal()
+  }
 
-  // console.log(animal, id)
+  const handleShare = async () => {
+    try {
+      // Check if the navigator supports the Share API
+      if (navigator.share) {
+        await navigator.share({
+          title: 'Animal Details',
+          text: `Check out the details of ${animal.name}: ${window.location.href}`,
+        })
+      } else {
+        // Fallback behavior (e.g., show a Snackbar with the shareable link)
+        setSnackbarOpen(true)
+      }
+    } catch (error) {
+      console.error('Error sharing:', error)
+    }
+  }
+
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return
+    }
+
+    setSnackbarOpen(false)
+  }
+
+  const handleSaveSuccessSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return
+    }
+
+    setSaveSuccessSnackbarOpen(false)
+  }
+
   return (
     <Card sx={{ maxWidth: 800, margin: 'auto', mt: 15 }}>
       <CardHeader title={`${animal.name}`} />
-      {/* <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      /> */}
       <img
         src={animal.image}
         alt={animal.name}
@@ -106,38 +124,28 @@ export default function AnimalDetails() {
         }}
       />
 
-      <CardContent>
-        {/* <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
-        </Typography> */}
-      </CardContent>
+      <CardContent>{/* Additional content can be added here */}</CardContent>
+
       <CardActions disableSpacing>
         <Button
           variant="contained"
           size="small"
-          // to='/adoptform'
-          onClick={navigateToAdoptForm}
           sx={{
             ml: 4,
             borderRadius: '10px',
             textTransform: 'capitalize',
-            backgroundColor: '#FFB800', // Default background color
-            color: 'black', // Text color
+            backgroundColor: '#FFB800',
+            color: 'black',
             fontWeight: 'bold',
             '&:hover': {
-              backgroundColor: '#FFB800', // Keep the same background color on hover
+              backgroundColor: '#FFB800',
             },
           }}
+          onClick={handleOpenModal}
         >
           Adopt
         </Button>
-        {/* <button className="adoptBtn" sx={{color:'red'}}>Adopt</button> */}
-        {/* <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton> */}
-        <IconButton aria-label="share">
+        <IconButton aria-label="share" onClick={handleShare}>
           <ShareIcon />
         </IconButton>
         <ExpandMore
@@ -149,38 +157,121 @@ export default function AnimalDetails() {
           <ExpandMoreIcon />
         </ExpandMore>
       </CardActions>
-      {/* expand here */}
+
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-            set aside for 10 minutes.
+          <Typography variant="h6" paragraph>
+            Animal Details:
           </Typography>
           <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes.
-            Transfer shrimp to a large plate and set aside, leaving chicken and
-            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-            onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            facilisi. Sed vel magna quis justo pharetra blandit non eget urna.
           </Typography>
           <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is
-            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-            shrimp and mussels, tucking them down into the rice, and cook again
-            without stirring, until mussels have opened and rice is just tender,
-            5 to 7 minutes more. (Discard any mussels that don&apos;t open.)
+            Vivamus auctor auctor eros, vel ullamcorper neque cursus ac.
+            Pellentesque habitant morbi tristique senectus et netus et malesuada
+            fames ac turpis egestas.
           </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then
-            serve.
+          <Typography paragraph>
+            Fusce commodo nisl nec risus volutpat, in tempus augue efficitur.
+            Curabitur sit amet sapien eu ipsum sagittis hendrerit vitae at
+            justo.
+          </Typography>
+          <Typography paragraph>
+            Quisque tincidunt libero vel fringilla dictum. Nunc vel elit
+            feugiat, tincidunt justo vel, lacinia arcu.
           </Typography>
         </CardContent>
       </Collapse>
+
+      {/* Modal */}
+      <Dialog open={openModal} onClose={handleCloseModal}>
+        <DialogTitle>Adopt Animal</DialogTitle>
+        <DialogContent>
+          <TextField
+            label=" Name"
+            type="text"
+            value={input1}
+            onChange={(e) => setInput1(e.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label=" Email address"
+            type="text"
+            value={input2}
+            onChange={(e) => setInput2(e.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label=" Phone number"
+            type="number"
+            value={input3}
+            onChange={(e) => setInput3(e.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label=" Address"
+            type="text"
+            value={input4}
+            onChange={(e) => setInput4(e.target.value)}
+            fullWidth
+            margin="normal"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleCloseModal}
+            color="primary"
+            sx={{
+              backgroundColor: '#f44336', // Red color for the button
+              color: 'white',
+              '&:hover': {
+                backgroundColor: '#d32f2f', // Darker red on hover
+              },
+            }}
+          >
+            Cancel
+          </Button>
+
+          <Button
+            onClick={handleSaveModal}
+            color="primary"
+            sx={{
+              backgroundColor: '#4CAF50', // Green color for the button
+              color: 'white',
+              '&:hover': {
+                backgroundColor: '#45a049', // Darker green on hover
+              },
+            }}
+          >
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      {/* Snackbar for share link */}
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleSnackbarClose}
+        message={`Shareable link: ${window.location.href}`}
+      />
+
+      {/* Snackbar for save success */}
+      <Snackbar
+        open={saveSuccessSnackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleSaveSuccessSnackbarClose}
+        message="Save successful!"
+        icon={<CheckCircleIcon fontSize="large" />}
+        sx={{
+          backgroundColor: '#4CAF50', // Green color for success
+          color: 'white',
+        }}
+      />
     </Card>
   )
 }
